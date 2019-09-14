eef75c9ee6cc1db08106e1b66d235fbb116f5794
dmitryavilov <d.av89@yandex.ru>
2019-09-14 15:50:58 +0400

    в

--- Avangard/js/main.js
+++ Avangard/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('.collapse__icon').click(function(event) {
+		$('body').toggleClass('dont_scroll');
 		$('.collapse__menu').toggleClass('header_collapse-menu_active');
 		$(this).toggleClass('collapse_icon_hover');
 		$('.overlay').toggleClass('overlay_active');

fae8b9d31b3220182fd3d929cdcff369f951d4bc
dmitryavilov <d.av89@yandex.ru>
2019-09-20 20:30:26 +0400

    в

--- Avangard/js/main.js
+++ Avangard/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$('.collapse__icon').click(function() {
 		$('body').toggleClass('dont_scroll');
 		$('.icon_2').toggleClass('icon_2_active');
@@ -18,4 +19,24 @@ $(document).ready(function() {
 		$('.icon_1').toggleClass('icon_1_active');
 		$('.icon_3').toggleClass('icon_3_active');
 	});
+
+	$('.benefits__advantage-1').mouseenter(function() {
+		$('.benefits__advantage-img-1').toggleClass('benefits_advantage_hover-spin');
+	});
+	$('.benefits__advantage-1').mouseleave(function() {
+		$('.benefits__advantage-img-1').toggleClass('benefits_advantage_hover-spin');
+	});
+	$('.benefits__advantage-2').mouseenter(function() {
+		$('.benefits__advantage-img-2').toggleClass('benefits_advantage_hover-spin');
+	});
+	$('.benefits__advantage-2').mouseleave(function() {
+		$('.benefits__advantage-img-2').toggleClass('benefits_advantage_hover-spin');
+	});
+	$('.benefits__advantage-3').mouseenter(function() {
+		$(this).queue(function () {$('.benefits__advantage-img-3').css('transform', 'rotate(-20deg)');$(this).dequeue();});
+		$(this).queue(function () {$('.benefits__advantage-img-3').css('transition', '0.3s');$(this).dequeue();});
+		$(this).delay(300).queue(function () {$('.benefits__advantage-img-3').css('transform', 'rotate(20deg)');$(this).dequeue();});
+		$(this).delay(400).queue(function () {$('.benefits__advantage-img-3').css('transform', 'rotate(0deg)');$(this).dequeue();});
+	});
+
 });

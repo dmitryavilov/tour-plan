fdcae7a595bee3a6f268d8b35d1524f3e44773d0
dmitryavilov <54320107+dmitryavilov@users.noreply.github.com>
2020-03-16 21:14:18 +0300

    d
    d

---
+++ yoga/js/index.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".stock-slider-block",{slidesPerView:"auto",slideActiveClass:"stock-slide-active",slideVisibleClass:"stock-slide-duplicate-active",slidesToScroll:1,centeredSlides:!0,loop:!0,initialSlide:1,slideClass:"stock-slide",spaceBetween:30,breakpoints:{710:{spaceBetween:30},400:{spaceBetween:15},320:{spaceBetween:12}}});

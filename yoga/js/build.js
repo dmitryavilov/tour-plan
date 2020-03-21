8bbce987a2e074bba85489d46a5a9937c57d3195
dmitryavilov <54320107+dmitryavilov@users.noreply.github.com>
2020-03-21 22:56:12 +0300

    в
    в

--- yoga/js/build.js
+++ yoga/js/build.js
@@ -1,32 +1,42 @@
-var swiper = new Swiper('.stock-slider-block', {
-  slidesPerView: 'auto',
-  slideActiveClass: 'stock-slide-active',
-  slideVisibleClass: 'stock-slide-duplicate-active',
-  slidesToScroll: 1,
-  loop: true,
-  centeredSlides: true,
-  initialSlide: 1,
-  slideClass: 'stock-slide',
-  spaceBetween: 30,
-  breakpoints: {
-    710: {
-      spaceBetween: 30
-    },
-    
-    400: {
-      spaceBetween: 15
-    },
+  const slBlocks = document.querySelectorAll('.stock-slider-block');
+  //CONST-STOCK-SLIDER-BLOCK
+//CONSTS
 
-    320: {
-      spaceBetween: 12
+
+  var swiper = new Swiper('.stock-slider-block', {
+    slidesPerView: 'auto',
+    slideActiveClass: 'stock-slide-active',
+    slideVisibleClass: 'stock-slide-duplicate-active',
+    slidesToScroll: 1,
+    loop: true,
+    centeredSlides: true,
+    initialSlide: 1,
+    slideClass: 'stock-slide',
+    spaceBetween: 30,
+    breakpoints: {
+      710: {
+        spaceBetween: 30
+      },
+      
+      400: {
+        spaceBetween: 15
+      },
+
+      320: {
+        spaceBetween: 12
+      }
     }
-  }
-});
+  });
+  //SLIDER-LIBRARY
 
-function onload(){
-  const slBlock = document.querySelector('.stock-slider-block').classList;
-  slBlock.add('slider-block_optimize');
-};
+  function onload(){
+    for(i = 0; i<slBlocks.length; i++){
+      slBlocks[i].classList.add('slider-block_optimize');
+    }
+  };  
+  //SLIDER-OPTIMIZE-FUNCTION
+
+//STOCK-SLIDER-BLOCK
 
 
 

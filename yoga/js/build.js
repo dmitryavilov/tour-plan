20e4deb0d01ee652105834ae4edd0e91b9932c68
dmitryavilov <54320107+dmitryavilov@users.noreply.github.com>
2020-03-30 23:34:02 +0300

    d
    d

--- yoga/js/build.js
+++ yoga/js/build.js
@@ -2,32 +2,50 @@
   //CONST-STOCK-SLIDER-BLOCK
 //CONSTS
 
+    var swiper = new Swiper('.stock-slider-block', {
+      slidesPerView: 'auto',
+      slideActiveClass: 'stock-slide-active',
+      slideVisibleClass: 'stock-slide-duplicate-active',
+      slidesToScroll: 1,
+      loop: true,
+      centeredSlides: true,
+      initialSlide: 1,
+      slideClass: 'stock-slide',
+      spaceBetween: 30,
+      breakpoints: {
+        710: {
+          spaceBetween: 30
+        },
+        
+        400: {
+          spaceBetween: 15
+        },
 
-  var swiper = new Swiper('.stock-slider-block', {
-    slidesPerView: 'auto',
-    slideActiveClass: 'stock-slide-active',
-    slideVisibleClass: 'stock-slide-duplicate-active',
-    slidesToScroll: 1,
-    loop: true,
-    centeredSlides: true,
-    initialSlide: 1,
-    slideClass: 'stock-slide',
-    spaceBetween: 30,
-    breakpoints: {
-      710: {
-        spaceBetween: 30
-      },
-      
-      400: {
-        spaceBetween: 15
-      },
-
-      320: {
-        spaceBetween: 12
+        320: {
+          spaceBetween: 12
+        }
       }
-    }
-  });
-  //SLIDER-LIBRARY
+    });
+    //STOCK-SLIDER
+
+    var swiper = new Swiper('.instructors-slider-block', {
+      slidesPerView: 'auto',
+      slideActiveClass: 'instructors-slide-active',
+      slidesToScroll: 1,
+      spaceBetween: 5,
+      slideClass: 'instructors-slide',
+
+      pagination: {
+        el: '.instructors-pagination',
+        type: 'bullets',
+        clickable: true,
+        bulletClass: 'instructors-pagination__bullet',
+        bulletActiveClass: 'instructors-pagination__bullet-active',
+        bulletElement: 'div'
+      }
+    });
+    //INSTRUCTORS-SLIDER
+//SLIDER-LIBRARY
 
   function onload(){
     for(i = 0; i<slBlocks.length; i++){
@@ -35,7 +53,6 @@
     }
   };  
   //SLIDER-OPTIMIZE-FUNCTION
-
 //STOCK-SLIDER-BLOCK
 
 

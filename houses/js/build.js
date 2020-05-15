f33cd52e357acf363caf3c5142f9cf1ee60ca28e
Dmitry <d.av89@yandex.ru>
2020-05-15 18:29:33 +0300

    d
    d

--- houses/js/build.js
+++ houses/js/build.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function(){
               menuBtn = document.querySelector('.phone-and-menu-block__menu-btn-wrapper');
         //HEADER
     //VARS
-
     const container = {
         template: `
         <div style="max-width:1140px; overflow:hidden; margin:0 auto; width: 95%">
@@ -75,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     textAlign: 'center'
                 },
                 menuElems: [
-                    {text: 'Наши услуги', link: '#'},
+                    {text: 'Наши услуги', link: '#block-1'},
                     {text: 'Рассчитать стоимость', link: '#'},
                     {text: 'Портфолио', link: '#'},
                     {text: 'Почему мы', link: '#'},
@@ -87,18 +86,156 @@ document.addEventListener('DOMContentLoaded', function(){
                 ]
             }        
         },
+        methods: {
+            menuToggle(){
+                document.querySelector('.navigation-block').classList.toggle('navigation-block_toggle');
+                document.querySelectorAll('.phone-and-menu-block__elem')[0].classList.toggle('phone-and-menu-block_elem_1_active');
+                document.querySelectorAll('.phone-and-menu-block__elem')[1].classList.toggle('phone-and-menu-block_elem_2_active');
+                document.querySelectorAll('.phone-and-menu-block__elem')[2].classList.toggle('phone-and-menu-block_elem_3_active');
+            }
+        },
         template: `
         <nav class="navigation-block navigation-block_pos">
             <div class="navigation-block__content" style="height: 95%; width:100%; overflow-y: auto; display:flex; align-items: center; justify-content: center">
                 <div class="navigation-block__content-wrapper" style="display:flex; flex-direction: column; align-items: center">
                     <p :style="styles" class="navigation-block__elem" v-for="elem in menuElems">
-                        <a :href="elem.link" style="text-decoration:none; color:white" class="navigation-block__link">{{elem.text}}</a>
+                        <a href="#" v-scroll-to=elem.link @click="menuToggle()" style="text-decoration:none; color:white" class="navigation-block__link">{{elem.text}}</a>
                     </p>
                 </div>
             </div>
         </nav>`
     }
+
+    const blockTitle = {
+        data(){
+            return {
+                blockStyles: {
+                    width: '100%',
+                    height: '56px',
+                    textAlign: 'center',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                },
+                lineStyles: {
+                    width: '377px',
+                    height: '1px'
+                }
+            }
+        },
+        props: {
+            text: String,
+            textColor: String,
+            lineColor: String
+        },
+        template: `
+        <div class="page-title-block" style="margin-top: 62px; margin-bottom: 30px" :style="blockStyles">
+            <h2 class="page-title-block__text" :style="{color: textColor}" style="font-size: 36px; font-family: 'Roboto';font-weight: 400">{{text}}</h2>
+            <div class="page-title-block__underline" :style="[lineStyles, {backgroundColor: lineColor}]"></div>
+        </div>`
+    }
+
+    const transferList = {
+        data(){
+            return {
+                blockStyles: {
+                    width: '100%',
+                    height: '340px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                },
+                titleStyles: {
+                    fontFamily: 'Roboto',
+                    fontWeight: 400,
+                    fontSize: '28px',
+                    color: '#19b5fe'
+                },
+                descriptionStyles: {
+                    fontFamily: 'Roboto',
+                    fontWeight: 300,
+                    fontSize: '18px',
+                    color: '#464646',
+                    lineHeight: '24px'
+                },
+                mainWrapperStyles: {
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start',
+                    height: '191px'
+                },
+                textWrapperStyles: {
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    height: '115px',
+                    width: '555px'
+                },
+                imgStyles: {
+                    width: '555px',
+                    height: '320px'
+                },
+                transfers: [
+                    {
+                        title: 'Дизайн интерьера',
+                        description: `
+                            Услуга, направленная на разработку интерьеров помещений с
+                            целью обеспечить удобство и эстетически приятное ощущение у
+                            человека при нахождении в помещении`,
+                        imgLink: 'img/transfer/1.jpg'
+                    },
+                    {
+                        title: 'Дизайн экстерьера',
+                        description: `
+                            Услуга, направленная на разработку дизайна прилегающих элементов 
+                            зданий, а также дизайн самих зданий, позволяющая обе 
+                            спечить удобство людей при нахождении на территории`,
+                        imgLink: 'img/transfer/2.jpg'
+                    },
+                    {
+                        title: 'Визуализация',
+                        description: `
+                            Услуга, направленная на визуализацию помещений с
+                            понять, как оно будет выглядеть в итоге. Это позволяет
+                            исключить ошибки при выборе дизайна`,
+                        imgLink: 'img/transfer/3.jpg'
+                    }
+                ]
+            }
+        },
+        template: `
+        <div class="transfer-list-block">
+            <div class="transfer-list-block__elem" v-for="transfer in transfers" :style="blockStyles">
+                <div class="transfer-list-block__main-wrapper" :style="mainWrapperStyles">
+                    <div class="transfer-list-block__text-wrapper" :style="textWrapperStyles">
+                        <h2 :style="titleStyles" class="transfer-list-block__title">{{transfer.title}}</h2>
+                        <p  :style="descriptionStyles" class="transfer-list-block__description">{{transfer.description}}</p>
+                    </div>
+                    <slot>
+                    
+                    </slot>
+                </div>
+               <img :src="transfer.imgLink" :style="imgStyles" class="transfer-list-block__img">
+            </div>
+        </div>`
+    }
     //COMPONENTS
+
+    VueScrollTo.setDefaults({
+        container: "body",
+        duration: 500,
+        easing: "ease",
+        offset: 0,
+        force: true,
+        cancelable: true,
+        onStart: false,
+        onDone: false,
+        onCancel: false,
+        x: false,
+        y: true
+    })
 
     new Vue({
         el: '.wrapper',
@@ -110,7 +247,9 @@ document.addEventListener('DOMContentLoaded', function(){
             container,
             textField,
             btn,
-            menuNavigation
+            menuNavigation,
+            blockTitle,
+            transferList
         },
         methods: {
             menuToggle(){
